03c1da23974f70b6fb5833ed1c375e8cc2054de6
Wholevinski <wholevinski@rhel6.(none)>
2013-11-06 10:47:45 -0500

    Refactored how we aggregate stats since we're split by snapshots now Added snapshot reader Added new graph generation and tests

--- analysis/analyze/digester.py
+++ analysis/analyze/digester.py
@@ -3,44 +3,67 @@ import numpy
 import os
 class Digester:
 
-	def load_json(self, json_str):
-		json_obj = json.loads(json_str)
-		queue_name = json_obj["queueName"]
-  
-	def process_statistics(self, stats):
-		processed_stats = {"intervalStats":[], "captureStats":[], "overallStats":{}}
-		stats = sorted(stats, key=lambda stat: stat["time"])
-
-		if(len(stats) > 1):
-			for i in range(1, len(stats)):
-				interval = {}
-				interval["msgInRate"] = float(stats[i]["msgIn"] - stats[i-1]["msgIn"]) / float((stats[i]["time"] - stats[i-1]["time"]) / 1000)
-				interval["msgOutRate"] = float(stats[i]["msgIn"] - stats[i-1]["msgIn"]) / float((stats[i]["time"] - stats[i-1]["time"]) / 1000)
-				interval["startTime"] = stats[i-1]["time"]
-				interval["endTime"] = stats[i]["time"]
-
-				processed_stats["intervalStats"].append(interval)
-
-			msg_sizes = []
-			for i in range(0, len(stats)):
-				capture = {}
-				capture["avgMessageSize"] = numpy.average(numpy.array(stats[i]["msgSizes"]))
-				capture["msgDepth"] = stats[i]["depth"]
-				capture["time"] = stats[i]["time"]
-				processed_stats["captureStats"].append(capture)
-				msg_sizes.append(stats[i]["msgSizes"])
-			
-			msg_sizes = numpy.array([item for sublist in msg_sizes for item in sublist])
-			processed_stats["overallStats"]["avgDepth"] = numpy.average(numpy.array([ stat["depth"] for stat in stats ]))
-			processed_stats["overallStats"]["avgMsgInRate"] = numpy.average(numpy.array([ stat["msgInRate"] for stat in processed_stats["intervalStats"] ]))
-			processed_stats["overallStats"]["avgMsgOutRate"] = numpy.average(numpy.array([ stat["msgOutRate"] for stat in processed_stats["intervalStats"] ]))
-			processed_stats["overallStats"]["maxMsgSize"] = numpy.max(msg_sizes)
-			processed_stats["overallStats"]["minMsgSize"] = numpy.min(msg_sizes)
-			processed_stats["overallStats"]["stdDevMsgSize"] = numpy.std(msg_sizes)
-			processed_stats["overallStats"]["allMsgSizes"] = msg_sizes
-
-			print processed_stats
-		else:
+	def read_snapshot_files(self, snapshot_dir):
+		all_snapshots = []
+		snapshots = os.listdir(snapshot_dir)
+		for snapshot in snapshots:
+			with open(os.path.join(snapshot_dir, snapshot), 'r') as f:
+				file_body = f.read()
+				all_snapshots.append(json.loads(file_body))
+		return all_snapshots
+  		
+	def generate_aggregate_stats(self, snapshots):
+		snapshots = sorted(snapshots, key=lambda snapshot: snapshot["time"])
+		if(len(snapshots) < 2):
 			raise ValueError("Not enough stats to extrapolate results")
 
+		processed_statistics = {}
+		for i in range(0, len(snapshots)):
+			captures = snapshots[i]["captures"]
+			totalMsgIn, totalMsgOut, totalMsgDepth = 0,0,0
+			msg_sizes = []
+			for j in range(len(captures)):
+				
+				totalMsgIn += captures[j]["msgIn"]
+				totalMsgOut += captures[j]["msgOut"]
+				totalMsgDepth += captures[j]["depth"]
+				msg_sizes.append([ msg["size"] for msg in captures[j]["msgs"] ])
+			
+			msg_sizes = [item for sublist in msg_sizes for item in sublist]
+			snapshots[i]["aggregateStats"] = { "totalMsgIn":totalMsgIn, "totalMsgOut":totalMsgOut, "totalMsgDepth":totalMsgDepth, "msgSizes":msg_sizes, "avgMessageSize":numpy.average(msg_sizes) }
+
+		#print snapshots
+		return snapshots
+
+	def process_aggregate_stats(self, snapshots):
+		processed_stats = {"intervalStats":[], "overallStats":{} }
+		snapshots = sorted(snapshots, key=lambda snapshot: snapshot["time"])
+		for j in range(1, len(snapshots)):
+			interval_stat = {}
+			start_time = snapshots[j-1]["time"]
+			end_time = snapshots[j]["time"]
+			interval = end_time - start_time
+
+			interval_stat["msgInRate"] = float(snapshots[j]["aggregateStats"]["totalMsgIn"] - snapshots[j-1]["aggregateStats"]["totalMsgIn"]) / float(interval / 1000)
+			interval_stat["msgOutRate"] = float(snapshots[j]["aggregateStats"]["totalMsgOut"] - snapshots[j-1]["aggregateStats"]["totalMsgOut"]) / float(interval / 1000)
+			interval_stat["startTime"] = start_time
+			interval_stat["endTime"] = end_time
+
+			processed_stats["intervalStats"].append(interval_stat)
+
+		all_msg_sizes = []
+		for j in range(0, len(snapshots)):
+			all_msg_sizes.append(snapshots[j]["aggregateStats"]["msgSizes"])
+		
+		all_msg_sizes = [item for sublist in all_msg_sizes for item in sublist]
+
+		processed_stats["overallStats"]["avgDepth"] = numpy.average(numpy.array([ snapshot["aggregateStats"]["totalMsgDepth"] for snapshot in snapshots ]))
+		processed_stats["overallStats"]["avgMsgInRate"] = numpy.average(numpy.array([ stat["msgInRate"] for stat in processed_stats["intervalStats"] ]))
+		processed_stats["overallStats"]["avgMsgOutRate"] = numpy.average(numpy.array([ stat["msgOutRate"] for stat in processed_stats["intervalStats"] ]))
+		processed_stats["overallStats"]["maxMsgSize"] = numpy.max(all_msg_sizes)
+		processed_stats["overallStats"]["minMsgSize"] = numpy.min(all_msg_sizes)
+		processed_stats["overallStats"]["stdDevMsgSize"] = numpy.std(all_msg_sizes)
+		processed_stats["overallStats"]["allMsgSizes"] = all_msg_sizes
+
+		print processed_stats
 		return processed_stats
--- analysis/analyze/graph_writer.py
+++ analysis/analyze/graph_writer.py
@@ -18,8 +18,18 @@ class GraphWriter:
 
 	def create_graphs(self, stats, test=False):
 		tmpdir = tempfile.mkdtemp()
-		print tmpdir
+		
+		self.create_msg_depth(stats, tmpdir)
+		self.create_msg_in_rate(stats, tmpdir)
+		self.create_msg_out_rate(stats, tmpdir)
 
+		if(test):
+			plt.show()
+
+		return tmpdir
+
+	def create_msg_depth(self, stats, tmpdir):
+		
 		bar_width = (stats["captureStats"][1]["time"] - stats["captureStats"][0]["time"]) / 10
 		x,y = [], []
 		for i in range(0,len(stats["captureStats"])):
@@ -33,7 +43,10 @@ class GraphWriter:
 		plt.grid(True)
 		plt.draw()
 		plt.savefig(os.path.join(tmpdir,"msg_depth.jpeg"))
-
+		
+	
+	def create_msg_in_rate(self, stats, tmpdir):
+		
 		bar_width = (stats["intervalStats"][0]["startTime"] - stats["intervalStats"][0]["endTime"]) / 10
 		x,y = [], []
 		for i in range(0,len(stats["intervalStats"])):
@@ -49,8 +62,42 @@ class GraphWriter:
 		plt.draw()
 		plt.savefig(os.path.join(tmpdir,"msg_in_rate.jpeg"))
 
-		if(test):
-			plt.show()
 
-		return tmpdir
+	def create_msg_out_rate(self, stats, tmpdir):
+		
+		bar_width = (stats["intervalStats"][0]["startTime"] - stats["intervalStats"][0]["endTime"]) / 10
+		x,y = [], []
+		for i in range(0,len(stats["intervalStats"])):
+			x.append(stats["intervalStats"][i]["startTime"])
+			y.append(stats["intervalStats"][i]["msgOutRate"])
+			
+		plt.figure()
+		plt.bar(x,y,width=bar_width)
+		plt.xlabel('Start Time')
+		plt.ylabel('Msg Rate (msgs/second)')
+		plt.title('Msg Out Rate Over Time')
+		plt.grid(True)
+		plt.draw()
+		plt.savefig(os.path.join(tmpdir,"msg_out_rate.jpeg"))
 
+	def create_msg_size_histogram(self, stats, tmpdir):
+		
+		plt.figure()
+		plt.xlabel('Msg Size')
+		plt.ylabel('Occurence')
+		plt.title('Msg Size Histogram')
+		plt.hist(stats["overallStats"]["allMsgSizes"])
+		plt.grid(True)
+		plt.draw()
+		plt.savefig(os.path.join(tmpdir,"msg_size_hist.jpeg"))
+
+	def create_msg_depth_histogram(self, stats, tmpdir):
+
+		plt.figure()
+		plt.xlabel('Msg Depth')
+		plt.ylabel('Occurence')
+		plt.title('Msg Depth Histogram')
+		plt.hist([ stat["msgDepth"] for stat in stats["captureStats"] ])
+		plt.grid(True)
+		plt.draw()
+		plt.savefig(os.path.join(tmpdir,"msg_depth_hist.jpeg"))
